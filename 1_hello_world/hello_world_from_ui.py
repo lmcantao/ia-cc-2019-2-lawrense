4b9a3d9ca01dafb3e2b26b3a02fe3f9225098b41
lmcantao <lawcantao@gmail.com>
2019-08-22 13:13:32 -0300

    Primeira tela

---
+++ 1_hello_world/hello_world_from_ui.py
@@ -0,0 +1,38 @@
+import sys, resource
+from PySide2 import QtCore
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication, QLabel, \
+    QPushButton, QAction
+from PySide2.QtCore import QFile
+
+
+def on_hello_pushbutton_clicked():
+    hello_label.setVisible(True)
+
+
+def on_action_reset_triggered():
+    hello_label.setVisible(False)
+
+
+if __name__ == "__main__":
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
+    app = QApplication(sys.argv)
+
+    ui_file = QFile("design/mainwindow.ui")
+    ui_file.open(QFile.ReadOnly)
+    loader = QUiLoader()
+    window = loader.load(ui_file)
+    ui_file.close()
+
+    hello_label = window.findChild(QLabel, 'helloLabel')
+    hello_label.setVisible(False)
+
+    hello_btn = window.findChild(QPushButton, 'helloPushButton')
+    hello_btn.clicked.connect(on_hello_pushbutton_clicked)
+
+    action_reset = window.findChild(QAction, 'actionReset')
+    action_reset.triggered.connect(on_action_reset_triggered)
+
+    window.show()
+
+    sys.exit(app.exec_())
